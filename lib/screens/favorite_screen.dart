b6e27d706f0806c056e15d1b09eb3fdf8458dee1
Muhammad Saad <s4saad2004@gmail.com>
2026-07-30 23:03:50 +0500

    New

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -17,7 +17,7 @@ class FavoriteScreen extends StatelessWidget {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
+          padding: const EdgeInsets.symmetric(horizontal: 29),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: const [
