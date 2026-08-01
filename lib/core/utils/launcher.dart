1a78bd45409c55b6d5d86d03efcadd2278fe1c60
Muhammad Umair Khan 108 <s4saad2004@gmail.com>
2026-08-01 14:41:44 +0500

    New Updated

--- lib/core/utils/launcher.dart
+++ lib/core/utils/launcher.dart
@@ -1,12 +1,11 @@
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchURL(String url) async {
+  if (url.isEmpty) return;
+
   final uri = Uri.parse(url);
 
-  if (!await launchUrl(
-    uri,
-    mode: LaunchMode.externalApplication,
-  )) {
+  if (!await launchUrl(uri)) {
     throw Exception('Could not launch $url');
   }
 }
